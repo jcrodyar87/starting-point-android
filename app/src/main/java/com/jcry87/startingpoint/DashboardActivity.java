27eb8d9c6eac1feaec26f1033698aef8e495e68b
Juan Carlos Rodriguez Yarmas <jcry87@gmail.com>
2022-12-29 23:10:16 -0500

    Add style noactionbar

--- app/src/main/java/com/jcry87/startingpoint/DashboardActivity.java
+++ app/src/main/java/com/jcry87/startingpoint/DashboardActivity.java
@@ -2,13 +2,34 @@ package com.jcry87.startingpoint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        Button btnTermsAndConditions = findViewById(R.id.btnTermsAndConditions);
+        btnLogin.setOnClickListener(this);
+        btnTermsAndConditions.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.btnLogin:
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+            case R.id.btnTermsAndConditions:
+                startActivity(new Intent(this, TermsAndConditionsActivity.class));
+                break;
+        }
     }
 }
