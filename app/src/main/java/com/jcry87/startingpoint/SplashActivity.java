248f6130fb7c78c5d9865686a25736fe27f87b9a
Juan Carlos Rodriguez Yarmas <jcry87@gmail.com>
2022-12-29 23:21:31 -0500

    Add splash activity

--- app/src/main/java/com/jcry87/startingpoint/SplashActivity.java
+++ app/src/main/java/com/jcry87/startingpoint/SplashActivity.java
@@ -2,7 +2,10 @@ package com.jcry87.startingpoint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            showMain();
+                },3000);
     }
+
+    private void showMain() {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+    
 }
