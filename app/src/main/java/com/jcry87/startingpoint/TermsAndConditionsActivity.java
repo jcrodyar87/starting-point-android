a78cef8c7473f56f62812cd8ee64d8b5150060dd
Juan Carlos Rodriguez Yarmas <jcry87@gmail.com>
2022-12-31 13:37:56 -0500

    Add data binding

--- app/src/main/java/com/jcry87/startingpoint/TermsAndConditionsActivity.java
+++ app/src/main/java/com/jcry87/startingpoint/TermsAndConditionsActivity.java
@@ -3,12 +3,25 @@ package com.jcry87.startingpoint;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class TermsAndConditionsActivity extends AppCompatActivity {
+import com.jcry87.startingpoint.databinding.ActivityTermsAndConditionsBinding;
 
+public class TermsAndConditionsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityTermsAndConditionsBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_terms_and_conditions);
+        //setContentView(R.layout.activity_terms_and_conditions);
+        binding = ActivityTermsAndConditionsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnClose.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        finish();
     }
 }
